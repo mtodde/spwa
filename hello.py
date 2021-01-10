adb4ffb43e721b14ab9b0568b329f188ad619f46
mtodde <77001092+mtodde@users.noreply.github.com>
2021-01-10 19:07:51 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 def hello_world():
     return "Houston, we've had a problem"
 
-app.run(host='0.0.0.0')
+#app.run(host='0.0.0.0') should not need this when launching with gunicorn
 #app.run(host='0.0.0.0', port=80)
 
 
